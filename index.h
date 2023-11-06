b1b72deb94442b3d3c68a825b44b51e97ead8ede
alex <alexbora@gmail.com>
2023-11-06 18:52:00 +0200

    mac Lun  6 Noi 2023 18:52:00 EET

--- index.h
+++ index.h
@@ -1,6 +1,13 @@
+
+
+#include <stddef.h>
 #include <stdint.h>
 
+#ifndef size_t
+typedef unsigned long size_t;
+#endif
+
 typedef struct {
-  char title[256];
+  uint8_t title[256];
   size_t len;
 } Buffer;
