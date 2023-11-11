b03c32122003ae81007b5de79e1b7d59eacee3b4
alex <alexbora@gmail.com>
2023-11-11 06:28:34 +0200

    mac Sâm 11 Noi 2023 06:28:34 EET

--- config.h
+++ config.h
@@ -1,4 +1,14 @@
+/**
+ * @author      : alex (alexbora@gmail.com)
+ * @file        : config
+ * @created     : Sâmbătă Noi 11, 2023 05:53:17 EET
+ */
+
 #ifndef CONFIG_H
+
 #define CONFIG_H
-#define USE_PROXY (1)
-#endif
+
+#define CONFIG_FILE
+
+#endif /* end of include guard CONFIG_H */
+
