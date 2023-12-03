e6c3fa002a3488dac6103c8987c459cc762fe462
alex <alexbora@gmail.com>
2023-12-03 09:38:12 +0200

    mac Dum  3 Dec 2023 09:38:12 EET

--- web_server/server_mac.c
+++ web_server/server_mac.c
@@ -3,6 +3,7 @@
 #include <arpa/inet.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <signal.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,8 +14,8 @@
 
 #define PORT 8080
 #define MAX_EVENTS 10
-#define INITIAL_TIMEOUT 1000 // Initial poll timeout in milliseconds
-#define WINDOW_SIZE 10       // Time window in seconds
+#define INITIAL_TIMEOUT 1000  // Initial poll timeout in milliseconds
+#define WINDOW_SIZE 10        // Time window in seconds
 
 #ifdef DEBUG
 #define BOLD_RED "\033[1;31m"
@@ -28,26 +29,26 @@ enum {
   ERROR,
 } level = INFO;
 
-#define debug(level, fmt, ...)                                                 \
-  do {                                                                         \
-    switch (level) {                                                           \
-    case INFO:                                                                 \
-      fprintf(stderr, "%s[INFO] %s:%d:%s(): " fmt, GREEN, __FILE__, __LINE__,  \
-              __func__, ##__VA_ARGS__);                                        \
-      fprintf(stderr, "%s\n", RESET);                                          \
-      break;                                                                   \
-    case ERROR:                                                                \
-      fprintf(stderr, "%s[ERROR] %s:%d:%s(): \n" fmt, BOLD_RED, __FILE__,      \
-              __LINE__, __func__, ##__VA_ARGS__);                              \
-      fprintf(stderr, "%s\n", RESET);                                          \
-      exit(EXIT_FAILURE);                                                      \
-      break;                                                                   \
-    case WARN:                                                                 \
-      fprintf(stderr, "%s[WARN] %s:%d:%s(): \n" fmt, YELLOW, __FILE__,         \
-              __LINE__, __func__, ##__VA_ARGS__);                              \
-      fprintf(stderr, "%s\n", RESET);                                          \
-      break;                                                                   \
-    }                                                                          \
+#define debug(level, fmt, ...)                                              \
+  do {                                                                      \
+    switch (level) {                                                        \
+      case INFO:                                                            \
+        fprintf(stderr, "%s[INFO] %s:%d:%s(): " fmt, GREEN, __FILE__,       \
+                __LINE__, __func__, ##__VA_ARGS__);                         \
+        fprintf(stderr, "%s\n", RESET);                                     \
+        break;                                                              \
+      case ERROR:                                                           \
+        fprintf(stderr, "%s[ERROR] %s:%d:%s(): \n" fmt, BOLD_RED, __FILE__, \
+                __LINE__, __func__, ##__VA_ARGS__);                         \
+        fprintf(stderr, "%s\n", RESET);                                     \
+        exit(EXIT_FAILURE);                                                 \
+        break;                                                              \
+      case WARN:                                                            \
+        fprintf(stderr, "%s[WARN] %s:%d:%s(): \n" fmt, YELLOW, __FILE__,    \
+                __LINE__, __func__, ##__VA_ARGS__);                         \
+        fprintf(stderr, "%s\n", RESET);                                     \
+        break;                                                              \
+    }                                                                       \
   } while (0)
 #else
 #define debug(...) (void)0
@@ -56,17 +57,25 @@ enum {
 #define ERROR (void)0
 #endif
 
-static inline void setnonblocking(int sockfd) {
-  int flags = fcntl(sockfd, F_GETFL, 0);
-  if (flags == -1)
-    debug(WARN, "Error getting socket flags");
-  if (fcntl(sockfd, F_SETFL, flags | O_NONBLOCK) == -1)
-    debug(WARN, "Error setting socket to non-blocking");
+int interrupted = 0;
+
+void sigint_handler(int sig) {
+  interrupted = 1;
+  debug(WARN, "SIGINT received");
+  exit(sig);
 }
 
-void handle_client(int client_socket) {
+static inline void setnonblocking(int sockfd) {
+  int flags = fcntl(sockfd, F_GETFL, 0);
+  if (flags == -1) debug(WARN, "Error getting socket flags");
+  if (fcntl(sockfd, F_SETFL, flags | O_NONBLOCK) == -1)
+    debug(WARN, "Error setting socket to non-blocking");
+  debug(INFO, "Socket set to non-blocking");
+}
+
+void handle_client(int client_socket, const char *file) {
 #define BUFFER_SIZE 1024
-  FILE *html_file = fopen("index.html", "r");
+  FILE *html_file = fopen(file, "r");
   if (html_file == NULL) {
     fprintf(stderr, "Error opening HTML file.\n");
     exit(EXIT_FAILURE);
@@ -78,6 +87,7 @@ void handle_client(int client_socket) {
   // Send HTTP header
   sprintf(response_buffer,
           "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n");
+
   send(client_socket, response_buffer, strlen(response_buffer), 0);
 
   // Send the content of index.html
@@ -90,7 +100,21 @@ void handle_client(int client_socket) {
 #undef BUFFER_SIZE
 }
 
+const char *handle_args(int argc, char **argv) {
+  return argc == 2 ? argv[1] : "index.html";
+}
+
 int main(int argc, char **argv) {
+  struct sigaction sa;
+  sa.sa_handler = sigint_handler;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = 0;
+  if (sigaction(SIGINT, &sa, NULL) == -1)
+    debug(WARN, "Error setting SIGINT handler");
+
+  const char *file = handle_args(argc, argv);
+  printf("file: %s\n", file);
+
   int serverSocket, clientSocket, kqueue_fd;
   struct sockaddr_in serverAddr, clientAddr;
   socklen_t addrSize = sizeof(struct sockaddr_in);
@@ -104,8 +128,7 @@ int main(int argc, char **argv) {
   serverAddr.sin_family = AF_INET;
   serverAddr.sin_addr.s_addr = INADDR_ANY;
   serverAddr.sin_port = htons(PORT);
-  if (serverAddr.sin_port == 0)
-    debug(ERROR, "Port htons failed");
+  if (serverAddr.sin_port == 0) debug(ERROR, "Port htons failed");
   //
   // Bind the socket
   if (bind(serverSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) ==
@@ -117,13 +140,11 @@ int main(int argc, char **argv) {
   setnonblocking(serverSocket);
 
   // Listen for incoming connections
-  if (listen(serverSocket, SOMAXCONN) == -1)
-    debug(ERROR, "Listen failed");
+  if (listen(serverSocket, SOMAXCONN) == -1) debug(ERROR, "Listen failed");
 
   // Create kqueue instance
   kqueue_fd = kqueue();
-  if (kqueue_fd == -1)
-    debug(ERROR, "Kqueue creation failed");
+  if (kqueue_fd == -1) debug(ERROR, "Kqueue creation failed");
 
   debug(INFO, "Kqueue created");
   // Register server socket for read events
@@ -140,94 +161,95 @@ int main(int argc, char **argv) {
   time_t start_time = time(NULL);
   int events_count = 0;
 
-  while (1) {
+  do {
     int num_events = kevent(kqueue_fd, NULL, 0, &event, 1, NULL);
 
     switch (num_events) {
-    case 0: {
-      time_t current_time = time(NULL);
-      double elapsed_time = difftime(current_time, start_time);
+      case 0: {
+        time_t current_time = time(NULL);
+        double elapsed_time = difftime(current_time, start_time);
 
-      if (elapsed_time >= WINDOW_SIZE) {
-        // Calculate events per second
-        double events_per_second = events_count / elapsed_time;
+        if (elapsed_time >= WINDOW_SIZE) {
+          // Calculate events per second
+          double events_per_second = events_count / elapsed_time;
 
-        // Adjust timeout based on events per second
-        timeout = events_per_second > 0 ? (int)(1000 / events_per_second)
-                                        : INITIAL_TIMEOUT;
-        // Reset counters
-        events_count = 0;
-        start_time = current_time;
-        debug(INFO, "Timeout adjusted to %d", timeout);
-      }
-    } break;
-
-    case -1:
-      debug(ERROR, "Kqueue wait error");
-      break;
-    default: {
-      if (event.ident == serverSocket) {
-        while (
-            (clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddr,
-                                   &addrSize)) != -1) {
-          debug(INFO, "Connection accepted from %s:%d",
-                inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
-
-          setnonblocking(clientSocket);
-          handle_client(clientSocket);
-
-          // Register client socket for read events
-          EV_SET(&change, clientSocket, EVFILT_READ, EV_ADD, 0, 0, NULL);
-          if (kevent(kqueue_fd, &change, 1, NULL, 0, NULL) == -1) {
-            debug(WARN, "Kevent registration failed for client socket");
-            close(clientSocket);
-            shutdown(clientSocket, SHUT_RDWR);
-          }
+          // Adjust timeout based on events per second
+          timeout = events_per_second > 0 ? (int)(1000 / events_per_second)
+                                          : INITIAL_TIMEOUT;
+          // Reset counters
+          events_count = 0;
+          start_time = current_time;
+          debug(INFO, "Timeout adjusted to %d", timeout);
         }
+      } break;
 
-        if (clientSocket == -1 && errno != EAGAIN && errno != EWOULDBLOCK)
-          debug(WARN, "Accept failed");
-
-      } else {
-        // Handle data from client
-        char buffer[1024];
-        ssize_t bytesRead;
-        while ((bytesRead = read(event.ident, buffer, sizeof(buffer))) > 0) {
-          debug(INFO, "Received data from client %zu: %.*s\n", event.ident,
-                (int)bytesRead, buffer);
-
+      case -1:
+        debug(ERROR, "Kqueue wait error");
+        break;
+      default: {
+        if (event.ident == serverSocket) {
           events_count++;
-          handle_client(clientSocket);
-        }
+          while ((clientSocket =
+                      accept(serverSocket, (struct sockaddr *)&clientAddr,
+                             &addrSize)) != -1) {
+            if (clientSocket == -1 && errno != EAGAIN && errno != EWOULDBLOCK)
+              debug(WARN, "Accept failed");
 
-        if (bytesRead == 0) {
-          // Connection closed by the client
-          debug(INFO, "Client %zu disconnected.\n", event.ident);
+            debug(INFO, "Connection accepted from %s:%d",
+                  inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
 
-          // Unregister client socket from kqueue
-          EV_SET(&change, event.ident, EVFILT_READ, EV_DELETE, 0, 0, NULL);
+            setnonblocking(clientSocket);
+            handle_client(clientSocket, file);
 
-          if (kevent(kqueue_fd, &change, 1, NULL, 0, NULL) == -1) {
-            debug(ERROR, "Kevent registration failed for server socket");
+            // Register client socket for read events
+            EV_SET(&change, clientSocket, EVFILT_READ, EV_ADD, 0, 0, NULL);
+            if (kevent(kqueue_fd, &change, 1, NULL, 0, NULL) == -1) {
+              debug(WARN, "Kevent registration failed for client socket");
+              close(clientSocket);
+              shutdown(clientSocket, SHUT_RDWR);
+            }
+          }
+
+        } else {
+          // Handle data from client
+          char buffer[1024];
+          ssize_t bytesRead;
+          while ((bytesRead = read(event.ident, buffer, sizeof(buffer))) > 0) {
+            debug(INFO, "Received data from client %zu: %.*s\n", event.ident,
+                  (int)bytesRead, buffer);
+
+            events_count++;
+            handle_client(clientSocket, file);
+          }
+
+          if (bytesRead == 0) {
+            // Connection closed by the client
+            debug(INFO, "Client %zu disconnected.\n", event.ident);
+
+            // Unregister client socket from kqueue
+            EV_SET(&change, event.ident, EVFILT_READ, EV_DELETE, 0, 0, NULL);
+
+            if (kevent(kqueue_fd, &change, 1, NULL, 0, NULL) == -1) {
+              debug(ERROR, "Kevent registration failed for server socket");
+              close(event.ident);
+            }
+          } else if (bytesRead == -1 && errno != (EAGAIN | EWOULDBLOCK)) {
+            debug(INFO, "Error reading data from client");
+
+            // Unregister client socket from kqueue
+            EV_SET(&change, event.ident, EVFILT_READ, EV_DELETE, 0, 0, NULL);
+            kevent(kqueue_fd, &change, 1, NULL, 0, NULL);
+
             close(event.ident);
           }
-        } else if (bytesRead == -1 && errno != (EAGAIN | EWOULDBLOCK)) {
-          debug(INFO, "Error reading data from client");
-
-          // Unregister client socket from kqueue
-          EV_SET(&change, event.ident, EVFILT_READ, EV_DELETE, 0, 0, NULL);
-          kevent(kqueue_fd, &change, 1, NULL, 0, NULL);
-
-          close(event.ident);
+          timeout = INITIAL_TIMEOUT;
+          debug(INFO, "Timeout reset to %d", timeout);
         }
+
+        // Reset timeout to initial value
         timeout = INITIAL_TIMEOUT;
         debug(INFO, "Timeout reset to %d", timeout);
-      }
-
-      // Reset timeout to initial value
-      timeout = INITIAL_TIMEOUT;
-      debug(INFO, "Timeout reset to %d", timeout);
-    } break;
+      } break;
     }
 
 #if 0
@@ -313,7 +335,7 @@ int main(int argc, char **argv) {
       break;
     }
 #endif
-  }
+  } while (1);
   // Close the server socket and kqueue instance (this part is unreachable
   // in this example)
   close(serverSocket);
